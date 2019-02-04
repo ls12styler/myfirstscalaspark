7904b39ac3e9bec05902a25b3430960b3272377f
Ashley Broadley <ashley@ls12style.co.uk>
2019-02-04 12:47:01 +0000

    A journey into Big Data with Apache Spark - Part 2

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "MyFirstScalaSpark"
+version := "1.0"
+scalaVersion := "2.11.12"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
---
+++ src/main/scala/MyFirstScalaSpark.scala
@@ -0,0 +1,27 @@
+package com.example
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.DataFrame
+import java.text.SimpleDateFormat
+
+object MyScalaApp {
+
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder
+      .appName("MyFirstScalaSpark")
+      .getOrCreate()
+
+    val filePath = args(0)
+    val data = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .option("timestampFormat", "dd/MM/yyyy")
+      .csv(filePath)
+
+    data.printSchema
+    data.show
+
+    val orderedData = data.orderBy(desc("Date of Payment"))
+    orderedData.show(5)
+  }
+}
